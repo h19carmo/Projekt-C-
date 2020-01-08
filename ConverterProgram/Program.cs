c831e7454bb89eec5ace13aae8f84e529440483f
h17mikmi <59643679+h17mikmi@users.noreply.github.com>
2020-01-08 11:28:15 +0100

    Senast
    Lunch!!

--- ConverterProgram/Program.cs
+++ ConverterProgram/Program.cs
@@ -22,50 +22,50 @@ namespace ConverterProgram
             "11.  Konvertera Gallon till Liter\n" + 
             "12.  Konvertera Liter till Gallon\n" + 
             "13.  Konvertera Timmar till Minuter\n" + 
-            "14.  Konvertera Minuter till Timmar" + 
+            "14.  Konvertera Minuter till Timmar\n" + 
             "0. Avsluta"; 
             
             ConverterService converter = new ConverterService();
             //Start point for the program.
             Console.WriteLine(str);
-            int val = 0;
+            int val;
             while(true){
                 while(!Int32.TryParse(Console.ReadLine(), out val)){
-                    Console.WriteLine("Felaktigt val\n" + str);
+                    Console.WriteLine("Felaktigt val {0}\n" + str, val);
                 }
                 switch(val){
                     case 1: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 2: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 3: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 4: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 5: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 6: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 7: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 8: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 9: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 10: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 11: 
-                    Console.WriteLine("{0}",val);
+                    Console.WriteLine("Du valde {0}",val);
                     break;
                     case 12: 
                     Console.WriteLine("{0}",val);
