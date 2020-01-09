a6ed818397b3bab783d21019fb34a75eb2ed03a8
MarcusForss <55194743+MarcusForss@users.noreply.github.com>
2020-01-09 15:17:02 +0100

    Småpill

--- ConverterProgram/Program.cs
+++ ConverterProgram/Program.cs
@@ -47,76 +47,91 @@ namespace ConverterProgram
                     case 1: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateAmps(ReadUserData("Ange spänningen"), ReadUserData("Ange resistansen"));
-                    Console.WriteLine("Strömmen blir {0}", result);
+                    Console.WriteLine("Strömmen blir {0} A", result);
+                    Console.ReadLine();
                     break;
                     case 2: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateResistance(ReadUserData ("Ange Spänningen"), ReadUserData("Ange ampere"));
                     Console.WriteLine("Resistansen blir {0}", result);
+                    Console.ReadLine();
                     break;
                     case 3: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateDiameterFromArea(ReadUserData("Ange area"));
                     Console.WriteLine("Diametern blir {0}", result);
+                    Console.ReadLine();
                     break;
                     case 4: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateRadiusFromArea(ReadUserData("Ange area"));
-                    Console.WriteLine("Radius blir {0}", result);
+                    Console.WriteLine("Radien blir {0}", result);
+                    Console.ReadLine();
                     break;
                     case 5: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateTravelTime(ReadUserData("Ange avståndet"), ReadUserData("Ange Hastigheten"));
                     Console.WriteLine("Resetiden blir {0}", result);
+                    Console.ReadLine();
                     break;
                     case 6: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateTraveledDistance(ReadUserData("Ange hastigheten"), ReadUserData("Ange tiden"));
                     Console.WriteLine("Avståndet blir {0}", result);
+                    Console.ReadLine();
                     break;
                     case 7: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateSpeed(ReadUserData("Ange tiden"), ReadUserData("Ange avståndet"));
                     Console.WriteLine("Hastigheten blir {0}", result);
+                    Console.ReadLine();
                     break;
                     case 8: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.CalculateVoltage(ReadUserData("Ange ampere"), ReadUserData("Ange resistans"));
-                    Console.WriteLine("Spänningen blir {0}", result);
+                    Console.WriteLine("Spänningen blir {0} V", result);
+                    Console.ReadLine();
                     break;
                     case 9: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.ConvertCelciusToFahrenheit(ReadUserData("Ange Celsius"));
-                    Console.WriteLine("Fahrenheit blir {0}", result);
+                    Console.WriteLine("Angivet värde blir {0} farhenheit", result);
+                    Console.ReadLine();
                     break;
                     case 10: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.ConvertFahrenheitToCelcius(ReadUserData("Ange Fahrenheit"));
-                    Console.WriteLine("Fahrenheit blir {0}", result);
+                    Console.WriteLine("Angivet värde blir blir {0} celcius", result);
+                    Console.ReadLine();
                     break;
                     case 11: 
                     Console.WriteLine("Du valde {0}",val);
                     result = converter.ConvertFromGallonToLiter(ReadUserData("Ange mängden gallon du vill konvertera till liter"));
-                    Console.WriteLine("Angiven gallon är {0} liter", result);
+                    Console.WriteLine("Angivna gallon är {0} liter", result);
+                    Console.ReadLine();
                     break;
                     case 12: 
                     Console.WriteLine("{0}",val);
                     result = converter.ConvertFromLiterToGallon(ReadUserData("Ange mängden liter du vill konvertera till gallon"));
                     Console.WriteLine("Angivna liter är {0} gallon", result);
+                    Console.ReadLine();
                     break;
                     case 13: 
                     Console.WriteLine("{0}",val);
                     result = converter.ConvertFromHoursToMinutes(ReadUserData("Ange antalet timmar du vill konvertera till minuter"));
-                    Console.WriteLine("Antalet minuter blir {0}", result);
+                    Console.WriteLine("Angivna timmar blir {0} minuter", result);
+                    Console.ReadLine();
                     break;
                     case 14: 
                     Console.WriteLine("{0}",val);
                     result = converter.ConvertFromMinutesToHours(ReadUserData("Ange antalet minuter du vill konvertera till timmar"));
-                    Console.WriteLine("Antalet timmar är {0}", result);
+                    Console.WriteLine("Angivna minuter blir {0} timmar", result);
+                    Console.ReadLine();
                     break;
                     case 0: return ;
                     default: 
                         Console.WriteLine("Feaktigt val försök igen!");
+                        Console.ReadLine();
                     break;
 
 
